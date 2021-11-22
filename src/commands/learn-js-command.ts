fdb8721c18f5ea614ee193466492b364762f8519
Matthew Olsson <matthewcolsson@gmail.com>
2021-11-22 11:44:39 -0700

    Remove extra spaces in learnjs command

--- src/commands/learn-js-command.ts
+++ src/commands/learn-js-command.ts
@@ -14,7 +14,7 @@ export default class LearnJSCommand extends Command {
             .setDescription(
                 trimIndent`
                     You need some basic JavaScript knowledge to use ChatTriggers, and the best place to do that is a \
-                    dedicated JavaScript tutorial. Here are some good places to start!
+                    dedicated JavaScript tutorial. Here are some good places to start! \
                     [Official Documentation](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
                     [Tutorial with Interactive Editor](https://www.codecademy.com/learn/introduction-to-javascript)
                 `,
