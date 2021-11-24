a13c99e26f7643e9203ef87a9fbb43ab259a2457
Matthew Olsson <matthewcolsson@gmail.com>
2021-11-24 12:56:05 -0800

    Prevent people from spamming botland

--- src/MessageListener.ts
+++ src/MessageListener.ts
@@ -1,4 +1,5 @@
 import { Message, PartialMessage } from 'discord.js';
+import Cache from 'node-cache';
 import { BOTLAND_CHANNEL_ID, NO_EMOJI_ROLE_ID, NO_QUOTES_ROLE_ID } from './config';
 
 type MaybePartialMessage = Message | PartialMessage;
@@ -9,10 +10,16 @@ const quoteRegex = /> .+\n<@[!&#]?[0-9]{16,18}>/;
 const containsEmoji = (message: string) => message.match(emojiRegex);
 const containsQuotes = (message: string) => message.match(quoteRegex);
 
+const botPlayerCache = new Cache();
+
 class MessageListener {
     onMessageCreate(message: Message) {
         if (message.channelId === BOTLAND_CHANNEL_ID && message.content === 'bot') {
-            message.reply('land');
+            const cacheEntry = botPlayerCache.get(message.author.id) as number | undefined;
+            if (!cacheEntry || cacheEntry < Date.now() - 10_000) {
+                message.reply('land');
+                botPlayerCache.set(message.author.id, Date.now());
+            }
         } else {
             const roles = [...(message.member?.roles.cache.values() ?? [])].map(role => role.id);
             if (roles.indexOf(NO_EMOJI_ROLE_ID) !== -1 && containsEmoji(message.content)) {
